0bcbe09fb77f67a3946c5931fa52e168d5e01a97
etorreborre <etorreborre@yahoo.com>
2013-02-14 15:10:10 +1100

    fixing the version search for the documentation

--- src/test/scala/com/nicta/scoobi/guide/ScoobiVariables.scala
+++ src/test/scala/com/nicta/scoobi/guide/ScoobiVariables.scala
@@ -49,7 +49,7 @@ trait ScoobiVariables {
   lazy val guidePage         = landingPage + guideDir
 
   private lazy val versionLine = buildSbt.flatMap(_.getLines.find(line => line contains "version"))
-  private def extractVersion(line: String) = "version\\s*\\:\\=\\s*\"(.*)\"".r.findFirstMatchIn(line).map(_.group(1))
+  private def extractVersion(line: String) = "\\s*version\\s*\\:\\=\\s*\"(.*)\"".r.findFirstMatchIn(line).map(_.group(1))
   private lazy val buildSbt = tryOr(Option(Source.fromFile("project/build.scala")))((e:Exception) => { println("can't find the project/build.scala file "+e.getMessage); None })
 
   implicit def toVersionedText(t: String): VersionedText = VersionedText(t)
